cce2bba3c1485533fccee19cda13de8f58e47d80
Yxiox <bruno.dela.goncalves@gmail.com>
2024-08-04 13:59:48 -0300

    Teste básico para criação de excel através do ClosedXML
    Adicionado exemplos básicos de comandos para a criação de uma tabela excel utilizando a biblioteca gratuita Closed XML.

---
+++ BancoTalentos.API/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+using FluentResults;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SenacPlataform.Shared.Controllers;
+
+namespace BancoTalentos.API.Controllers
+{
+    [Route("excel")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [GetAll]
+        public async Task<IActionResult> teste()
+        {
+            string path = System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\Downloads\\Teste2.xlsx";
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Sample Sheet");
+                worksheet.Cell("A1").Value = "Teste!";
+                worksheet.Cell("A2").Value = "Teste 2";
+                workbook.SaveAs(path);
+
+                
+
+
+            }
+            
+
+            return Ok("Sucesso truta");
+        }
+
+
+    }
+}
