5c15d6885d111628e58c60c3f1fc4b9260e2aac9
Victor Santos <110687512+VictorSantos09@users.noreply.github.com>
2024-08-05 19:47:35 -0300

    KAN-28

--- BancoTalentos.Domain/Services/Pessoas/Professores/Interfaces/IConsultaProfessorService.cs
+++ BancoTalentos.Domain/Services/Pessoas/Professores/Interfaces/IConsultaProfessorService.cs
@@ -1,4 +1,5 @@
 ﻿using BancoTalentos.Domain.Entity;
+using BancoTalentos.Domain.Services.Imagem.Dto;
 using FluentResults;
 
 namespace BancoTalentos.Domain.Services.Pessoas.Professores.Interfaces;
@@ -7,4 +8,5 @@ public interface IConsultaProfessorService
 {
     Task<Result<IEnumerable<PESSOAS>>> GetAllAsync(CancellationToken cancellationToken = default);
     Task<Result<PESSOAS>> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<Result<ImagemDTO>> GetFotoPerfilAsync(int id, CancellationToken cancellationToken = default);
 }
