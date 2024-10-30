527149b8967c3642549b55837bbc018630f59ba0
Victor Santos <110687512+VictorSantos09@users.noreply.github.com>
2024-10-30 19:59:15 -0300

    Criado Extensão para Configuração AppSettings

---
+++ SenacPlataform.Shared/Config/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SenacPlataform.Shared.Config;
+
+public static class ConfigurationBuilderExtensions
+{
+    // Consultar -> https://andrewlock.net/sharing-appsettings-json-configuration-files-between-projects-in-asp-net-core/#the-initial-setup
+
+    private static readonly string APPSETTINGS_PATH = $"{TryGetSolutionDirectory()}/appsettings.json";
+    private static readonly string APPSETTINGS_DEVELOPMENT_PATH = $"{TryGetSolutionDirectory()}/appsettings.development.json";
+    private const string NOME_PROJETO_SHARED = "SenacPlataform.Shared";
+    
+    public static IConfigurationBuilder SNConfigureAppSettings(this IConfigurationBuilder builder)
+    {
+        builder.AddJsonFile(APPSETTINGS_PATH, optional: false, reloadOnChange: false);
+        builder.AddJsonFile(APPSETTINGS_DEVELOPMENT_PATH, optional: true, reloadOnChange: false);
+        return builder;
+    }
+
+    private static DirectoryInfo TryGetSolutionDirectory()
+    {
+        return new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "..", NOME_PROJETO_SHARED));
+    }
+}
