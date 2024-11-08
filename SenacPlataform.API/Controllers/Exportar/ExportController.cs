7cbdc72b260962188cdd5d466f407ebd35653127
Victor Santos <110687512+VictorSantos09@users.noreply.github.com>
2024-11-07 21:01:41 -0300

    Exportação CSV/Excel

---
+++ SenacPlataform.API/Controllers/Exportar/ExportController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickKit.Shared.Export;
+using QuickKit.Shared.Generator.Csv;
+using QuickKit.Shared.Generator.Excel;
+using System.Text;
+
+namespace RadzenBlazorDemos
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public partial class ExportController : ControllerBase
+    {
+        [HttpPost("csv")]
+        public IActionResult ExportToCsv([FromBody] ExportRequest exportRequest)
+        {
+            var csvData = CsvGenerator.Generate(exportRequest.Data, exportRequest.Columns);
+            return File(new UTF8Encoding().GetBytes(csvData), "text/csv", $"{exportRequest.FileName}.csv");
+        }
+
+        [HttpPost("excel")]
+        public IActionResult ExportToExcel([FromBody] ExportRequest exportRequest)
+        {
+            var result = ExcelGenerator.Generate(exportRequest.Data, exportRequest.Columns);
+            return new FileStreamResult(result, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+            {
+                FileDownloadName = $"{exportRequest.FileName}.xlsx"
+            };
+        }
+    }
+}
---
+++ SenacPlataform.Shared/Controllers/ExportEndpoint.cs
@@ -0,0 +1,8 @@
+﻿namespace SenacPlataform.Shared.Controllers;
+
+public readonly struct ExportEndpoint
+{
+    public static string CSV { get; } = $"{BASE_URL}/csv";
+    public static string EXCEL { get; } = $"{BASE_URL}/excel";
+    private const string BASE_URL = "http://localhost:5183/api/export";
+}
